3111c8ebe48a58d403fa57139d895d840c97b660
sapphirekuo <sapphirekuo@gmail.com>
2017-12-08 13:51:48 +0800

    add restaurant new and create actions and views for admin-update redirect routes for create actions

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -15,15 +15,17 @@ class Admin::RestaurantsController < ApplicationController
     @restaurant = Restaurant.new(restaurant_params)
     if @restaurant.save
       flash[:notice] = "restaurant was successfully created"
+      redirect_to admin_restaurants_path
     else
       flash[:alaert] = "reataurant was failed to create"
+      render :new
     end
   end
 
   private
 
   def restaurant_params
-    params.require(:reataurant).permit(:name, :opening_hours, :tel, :address, :description)
+    params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description)
   end
-  
+
 end
