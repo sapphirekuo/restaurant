b0e0849b850e041b62cd544e68154afd5031aca7
sapphirekuo <sapphirekuo@gmail.com>
2018-01-08 11:35:40 +0800

    generate category model and add category_id to restaurants table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171214063547) do
+ActiveRecord::Schema.define(version: 20180108032919) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -21,6 +27,7 @@ ActiveRecord::Schema.define(version: 20171214063547) do
     t.string "opening_hours"
     t.text "description"
     t.string "image"
+    t.integer "category_id"
   end
 
   create_table "users", force: :cascade do |t|
