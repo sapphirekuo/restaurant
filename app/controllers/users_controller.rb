e9b004b1b67b249fea592a1e2f27d57cff8982b2
sapphirekuo <sapphirekuo@gmail.com>
2018-01-18 02:59:53 +0800

    user can update profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,34 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
 
   def show
-    @user = User.find(params[:id])
-
   end
 
+  def edit
+    if @user != current_user
+      flash[:alert] = "you can only edit your own profile"
+      redirect_to user_path(@user)
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "user was successfully updated"
+      redirect_to user_path(@user)
+    else
+      flash[:alert] = "user was failed to update"
+      render:edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :intro, :avatar)
+  end
 end
