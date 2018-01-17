f652801746034b5197d3e027f6108bad652734db
sapphirekuo <sapphirekuo@gmail.com>
2018-01-18 03:07:22 +0800

    update avatar uploader

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  mount_uploader :avatar, PhotoUploader
+  mount_uploader :avatar, AvatarUploader
 
   has_many :comments, dependent: :destroy
 
