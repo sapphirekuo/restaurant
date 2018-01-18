4be047cb88ec1baf4100d7ce7b7f3bf52be8a018
sapphirekuo <sapphirekuo@gmail.com>
2018-01-18 10:15:23 +0800

    added user name into Devise params and sing-up form, changed displayed user email to user name.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,20 @@ class User < ApplicationRecord
 
   has_many :comments, dependent: :destroy
 
+  # if name is the required field
+  #   validates_presence_of :name
+
+  # or use the name part of email as default name
+  before_save :default_name
+  before_update :default_name
+
+  def default_name
+    if self.name == '' || self.name == nil
+      self.name = self.email.split('@').first
+    end
+  end
+
+
   def admin?
     self.role == "admin"
   end
