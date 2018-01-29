96c4cdf323f94f427049745a448da510cf9aa565
sapphirekuo <sapphirekuo@gmail.com>
2018-01-29 15:41:41 +0800

    added user index page with follow button, validated uniqueness of following_id in user_id scope, added create action for followshiprecord.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
 
+  def index
+    @users = User.all
+  end
 
   def show
     @commented_restaurants = @user.restaurants.uniq
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ApplicationRecord
   has_many :liked_restaurants, through: :likes, source: :restaurant
 
   # 「使用者追蹤美食達人」的多對多關聯
-  has_many :followships, dependent :destroy
+  has_many :followships, dependent: :destroy
   has_many :followings, through: :followships
 
   # if name is the required field
